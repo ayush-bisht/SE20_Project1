88a428cac8a15862cf47850427c462f36b5cbf23
Vijay Anand <goodleader007@gmail.com>
2020-10-26 22:55:43 -0400

    started jest test case

--- social_media/functions/util/validators.js
+++ social_media/functions/util/validators.js
@@ -1,24 +1,38 @@
+
 const isEmail = (email) => {
-  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (email.match(regEx)) return true;
-  else return false;
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 };
 
 const hasNoWhiteSpace = (string) => {
+  if (string.trim() === string) return false;
+  else return true;
+};
+
+const isNotEmpty = (string) => {
   if (string.trim() === "") return true;
   else return false;
 };
 
-exports.validateSignupData = (data) => {
+const passwordLength = (string) => {
+  if (string.length > 6) return false;
+  else return true;
+};
+
+
+const validateSignupData = (data) => {
   let errors = {};
 
-  if (hasNoWhiteSpace(data.email)) {
+  if (isNotEmpty(data.email)) {
     errors.email = "Must not be empty";
+  } else if (hasNoWhiteSpace(data.email)) {
+    errors.email = "Should not contain white spaces";
   } else if (!isEmail(data.email)) {
     errors.email = "Must be a valid email address";
   }
 
   if (hasNoWhiteSpace(data.password)) errors.password = "Must not be empty";
+  if (passwordLength(data.password)) errors.password = "length should be atleast 7";
   if (data.password !== data.confirmPassword)
     errors.confirmPassword = "Passwords must match";
   if (hasNoWhiteSpace(data.handle)) errors.handle = "Must not be empty";
@@ -29,7 +43,7 @@ exports.validateSignupData = (data) => {
   };
 };
 
-exports.validateLoginData = (data) => {
+const validateLoginData = (data) => {
   let errors = {};
 
   if (hasNoWhiteSpace(data.email)) errors.email = "Must not be empty";
@@ -40,6 +54,8 @@ exports.validateLoginData = (data) => {
     valid: Object.keys(errors).length === 0 ? true : false,
   };
 };
+
+module.exports = { isEmail, hasNoWhiteSpace, isNotEmpty, passwordLength, validateSignupData, validateLoginData };
 
 exports.reduceUserDetails = (data) => {
   let userDetails = {};
